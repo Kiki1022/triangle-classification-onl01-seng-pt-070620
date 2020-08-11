258351203be2d86f18dc08cb33357a43b377b0ca
Kinnari Shah <kinnari1022@yahoo.com>
2020-08-11 02:28:10 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,39 @@
+require 'pry'
 class Triangle
-  # write code here
+ attr_accessor :a, :b, :c, :kind
+
+  def initialize (a,b,c)
+    @a = a 
+    @b = b 
+    @c = c
+  end
+
+  
+  def kind 
+    #binding.pry
+    if (a*b*c)==0 ||(a + b) <= c || (c + b) <= a || (c + a) <= b
+      
+      begin 
+        raise TriangleError
+        #rescue TriangleError => error 
+        puts error.message
+      end
+    
+    elseif a == b && b == c
+      self.kind = :equilateral
+    elseif a == b || a == c || b == c
+      self.kind = :isosceles
+    else
+      self.kind = :scalene
+  end
+end
+end
+
+ 
+ 
+ 
+  class TriangleError < StandardError
+    def message
+      puts "Not a Triangle!"
+  end
 end
